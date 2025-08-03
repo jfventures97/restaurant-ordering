d518a8b297e8c5916c39b1c6b13ea57bcc0574b2
jfventures97 <166265786+jfventures97@users.noreply.github.com>
2025-08-03 22:08:05 +0800

    Version 1.1.1 Removed margins from menuitems ( h1, h2, h3 )

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ console.log(menuArray)
 
 const menuData= menuArray.map(menu => {
   return `
+  <div class=menuitems>
   <div> ${menu.emoji} </div>
   <div>
     <h1>${menu.name}</h1>
     <h2>${menu.ingredients}</h2>
     <h3>${menu.price}</h3>
     </div>
-    <div> <i class="fa-solid fa-plus"></i></div> `}).join('')
-    
+    <div> <i class="fa-solid fa-plus"></i></div>
+    </div> `}).join('')
+
 menuContainer.innerHTML = menuData;
