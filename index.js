c9546c51b7bacf2917ac0f653e18ce5a324a8441
jfventures97 <166265786+jfventures97@users.noreply.github.com>
2025-08-04 20:52:35 +0800

    Version 1.1.6 Used querySelectorAll to select all .symbolsection. Added a forEach and eventlistener to all plus buttons to log out message

--- index.js
+++ index.js
@@ -16,8 +16,23 @@ const menuData= menuArray.map(menu => {
     <h2 class="menuingredients headerfont">${menu.ingredients}</h2>
     <h3 class= "menuprice headerfont">${menu.price}</h3>
     </div>
-    <div class="symbolsection"> <i class="fa-solid fa-plus symbol"></i></div>
+    <div class="symbolsection" id="symbolsection"> <i class="fa-solid fa-plus symbol"></i></div>
     </div>
     </div> `}).join('')
 
 menuContainer.innerHTML = menuData;
+
+
+const symbolButtons = document.querySelectorAll('.symbolsection')
+
+symbolButtons.forEach(button => {
+    button.addEventListener('click',() => {
+      console.log('You have clicked a button!')
+    })
+})
+
+// document.querySelectorAll('.symbolsection').forEach(button => {
+//   button.addEventListener('click', function(){
+//     console.log('Plus button Clicked!');
+//   });
+// });
