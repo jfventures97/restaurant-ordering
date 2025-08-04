d763f3ad98b57446478c216f030257c329b6eb00
jfventures97 <166265786+jfventures97@users.noreply.github.com>
2025-08-04 10:09:27 +0800

    Version 1.1.3 Completed Styling for Menu Part

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ console.log(menuArray)
 
 const menuData= menuArray.map(menu => {
   return `
+  <div class="menuOuter">
   <div class="menuitems">
   
   <div class="emoji"> ${menu.emoji} </div>
@@ -15,7 +16,8 @@ const menuData= menuArray.map(menu => {
     <h2 class="menuingredients headerfont">${menu.ingredients}</h2>
     <h3 class= "menuprice headerfont">${menu.price}</h3>
     </div>
-    <div> <i class="fa-solid fa-plus"></i></div>
+    <div class="symbolsection"> <i class="fa-solid fa-plus symbol"></i></div>
+    </div>
     </div> `}).join('')
 
 menuContainer.innerHTML = menuData;
